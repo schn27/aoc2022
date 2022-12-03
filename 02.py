7261af7f361e348ba6776350a21ea1498201caa6
Aleksandr Malikov <schn27@gmail.com>
2022-12-03 10:38:32 +0300

    refactor day 02

--- 02.py
+++ 02.py
@@ -1,4 +1,4 @@
-from aocd import data
+from aocd import lines
 from aocd import submit
 
 shape_score = {"A": 1, "B": 2, "C": 3}
@@ -8,20 +8,18 @@ outcome = {
     "B" : {"A": 0, "B": 3, "C": 6},
     "C" : {"A": 6, "B": 0, "C": 3}}
 
-def score(he_you):
+def score(line):
     shape_coding = {"X": "A", "Y": "B", "Z": "C"}
-    he, you = he_you
+    he, you = line.split()
     you = shape_coding[you]
     return shape_score[you] + outcome[he][you]
 
-def score2(he_result):
+def score2(line):
     result_coding = {"X": 0, "Y": 3, "Z": 6}
-    he, result = he_result
+    he, result = line.split()
     result = result_coding[result]
     shape = next(filter(lambda e: e[1] == result, outcome[he].items()))[0]
     return shape_score[shape] + result
 
-strategy = list(map(lambda e: e.split(), data.split("\n")))
-
-submit(sum(map(score, strategy)), part="a")
-submit(sum(map(score2, strategy)), part="b")
+submit(sum(map(score, lines)), part="a")
+submit(sum(map(score2, lines)), part="b")

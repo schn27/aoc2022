16fd918a09b413e2223ba177d63076cb549bc65c
Aleksandr Malikov <schn27@gmail.com>
2022-12-07 10:42:11 +0300

    refactor day 07

--- 07.py
+++ 07.py
@@ -1,42 +1,28 @@
 from aocd import lines
 from aocd import submit
 
-root = {}
-current = root
-
-def cd(current, child):
-    if child == '/':
-        return root
-
-    if not(child in current):
-        current[child] = {'..': current}
-
-    return current[child]
+dirs = {'': 0}
+current = ['']
 
 for line in lines:
     w = line.split()
     if w[0] == '$':
         if w[1] == 'cd':
-            current = cd(current, w[2])
+            if w[2] == '/':
+                current = ['']
+            elif w[2] == '..':
+                current = current[:-1]
+            else:
+                current.append(w[2])
     elif w[0] != 'dir':
-        current[w[1]] = int(w[0])
+        for i in range(len(current)):
+            dir_name = '/'.join(current[:i + 1])
+            dirs[dir_name] = int(w[0]) + (dirs[dir_name] if dir_name in dirs else 0)
 
-def get_sizes(current, sizes):
-    total = 0
-
-    for k, v in current.items():
-        if k != '..':
-            total += v if isinstance(v, int) else get_sizes(v, sizes)
-
-    sizes.append(total)
-    return total
-
-sizes = []
-total_size = get_sizes(root, sizes)
+sizes = list(sorted(dirs.values()))
 
 submit(sum(filter(lambda e: e <= 100000, sizes)), part='a')
 
-sizes.sort()
-goal = 30000000 - (70000000 - total_size)
+goal = 30000000 - (70000000 - sizes[-1])
 
 submit(next(filter(lambda e: e >= goal, sizes)), part='b')

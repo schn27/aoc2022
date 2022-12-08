18970d09b6e4bb014684ba9f3cbfea6f945b0f9d
Aleksandr Malikov <schn27@gmail.com>
2022-12-08 13:24:12 +0300

    refactor day 08

--- 08.py
+++ 08.py
@@ -1,53 +1,25 @@
 from aocd import lines
 from aocd import submit
 
+def get_visibility_and_count(line):
+    for i, e in enumerate(line[1:]):
+        if e >= line[0]:
+            return (False, i + 1)
+
+    return (True, len(line) - 1)
+
 trees = list(map(lambda e: list(map(int, e)), lines))
 
 visible = 0
 max_score = 0
 
-for r in range(len(trees)):
-    for c in range(len(trees[0])):
-        is_visible = False
-
-        cc = c - 1
-        left = 0
-        while (cc >= 0 and trees[r][c] > trees[r][cc]):
-            cc -= 1
-            left += 1
-
-        from_left = cc < 0
-        left += int(not from_left)
-
-        cc = c + 1
-        right = 0
-        while (cc < len(trees[0]) and trees[r][c] > trees[r][cc]):
-            cc += 1
-            right += 1
-
-        from_right = cc >= len(trees[0])
-        right += int(not from_right)
-
-        rr = r - 1
-        up = 0
-        while (rr >= 0 and trees[r][c] > trees[rr][c]):
-            rr -= 1
-            up += 1
-
-        from_up = rr < 0
-        up += int(not from_up)
-
-        rr = r + 1
-        down = 0
-        while (rr < len(trees) and trees[r][c] > trees[rr][c]):
-            rr += 1
-            down += 1
-
-        from_down = rr >= len(trees)
-        down += int(not from_down)
-
-        visible += int(from_left or from_right or from_up or from_down)
-        max_score = max(max_score, left * right * up * down)
+for r, row in enumerate(trees):
+    for c, _ in enumerate(row):
+        col = list(map(lambda e: e[c], trees))
+        dirs = [list(reversed(row[:c + 1])), row[c:], list(reversed(col[:r + 1])), col[r:]]
+        visibility, count = list(zip(*map(get_visibility_and_count, dirs)))
+        visible += int(any(visibility))
+        max_score = max(max_score, count[0] * count[1] * count[2] * count[3])
 
 submit(visible, part='a')
 submit(max_score, part='b')

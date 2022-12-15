75802e260e073dd0cf379f0c8cded6ae0f6b2d71
Aleksandr Malikov <schn27@gmail.com>
2022-12-15 12:57:50 +0300

    day 15

---
+++ 15.py
@@ -0,0 +1,40 @@
+from aocd import lines
+from aocd import submit
+import re
+
+sensors = list(map(lambda e: list(map(int, re.findall(r'-?\d+', e))), lines))
+
+def get_sensors_in_range(x, y):
+    return len(list(filter(lambda e:
+        abs(x - e[0]) + abs(y - e[1]) <= abs(e[2] - e[0]) + abs(e[3] - e[1]), sensors)))
+
+def get_invisible_pos(sx, sy, d, xy_checker):
+    x = sx
+    y = sy - d - 1
+    for dx, dy in [(1, 1), (-1, 1), (-1, -1), (1, -1)]:
+        for i in range(d):
+            if xy_checker(x, y) and get_sensors_in_range(x, y) == 0:
+                return x, y
+            x += dx
+            y += dy
+    return None
+
+row = 2000000
+free_positions = set()
+
+size = 4000000
+invisible_pos = None
+
+for sx, sy, bx, by in sensors:
+    d = abs(bx - sx) + abs(by - sy)
+    for dx in range(-d + abs(row - sy), d - abs(row - sy) + 1):
+        x = sx + dx
+        if row != by or x != bx:
+            free_positions.add(x)
+
+    if invisible_pos is None:
+        invisible_pos = get_invisible_pos(sx, sy, d,
+            lambda x, y: x >= 0 and x <= size and y >= 0 and y <= size)
+
+submit(len(free_positions), part='a')
+submit(invisible_pos[0] * size + invisible_pos[1], part='b')
